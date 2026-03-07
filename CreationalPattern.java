6bb3d98ce0b109663db7139e23192342449278b7
10nayan <nayanjyotipatowary@gmail.com>
2026-03-08 00:00:49 +0530

    new pattern added

--- CreationalPattern.java
+++ CreationalPattern.java
@@ -1,3 +1,12 @@
+/*
+Creational design patterns
+1. Singleton
+2. Factory
+3. Builder
+4. Abstract Factory
+5.Prototype
+ */
+
 public class CreationalPattern {
     public static void main(String[] args) {
         System.out.println("These are creational design pattern");
